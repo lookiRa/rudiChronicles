383742a86b7d30ab60b28c400fe46b1abf18d3c5
littleDreamer <40472265+Rampatunity@users.noreply.github.com>
2018-08-01 22:22:39 +0100

    ee-removal

--- public_html/themes/ee/asset/javascript/compressed/components/no_results.js
+++
@@ -1,8 +0,0 @@
-"use strict";/**
- * ExpressionEngine (https://expressionengine.com)
- *
- * @link      https://expressionengine.com/
- * @copyright Copyright (c) 2003-2018, EllisLab, Inc. (https://ellislab.com)
- * @license   https://expressionengine.com/license
- */
-function NoResults(e){return React.createElement("label",{className:"field-empty",dangerouslySetInnerHTML:{__html:e.text}})}
